3db594e1807614865dd0b7448e74cc344b7ae933
delmo1501 <bdelmoro@primerosystems.com>
2022-05-13 19:51:58 +0200

    Spinner, Fixing when the client doenst exist

--- src/paginas/VerCliente.jsx
+++ src/paginas/VerCliente.jsx
@@ -1,57 +1,75 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 const VerCliente = () => {
+  const [cliente, setCliente] = useState({});
+  const [cargando, setCargando] = useState(false); //para evitar el render de la api cada vez que refrescamos
 
-  const [cliente, setCliente] = useState({})
-  const {id} = useParams()
+  const { id } = useParams();
 
   useEffect(() => {
     const obtenerClienteAPI = async () => {
       try {
-        const url = `http://localhost:4000/clientes/${id}`
-        const respuesta = await fetch(url)
-        const resultado = await respuesta.json()
-        setCliente(resultado)
-
+        setCargando(!cargando);
+        const url = `http://localhost:4000/clientes/${id}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setCliente(resultado);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-    obtenerClienteAPI()
-  }, [])
-  
+      setCargando(false) // estos dos setCargando son muy importantes, porque si no queda renderizando, no cambia el estado y
+       //el spinner se queda ahi
+    };
+    obtenerClienteAPI();
+  }, []);
 
-  console.log(id)
+  console.log(id);
 
   return (
+    cargando ? <Spinner /> : 
+    Object.keys(cliente).length === 0 ?
+    `The client is empty` : 
+    
+    (
+
     <div>
-      <p className='font-black text-4xl text-blue-900'>Information about:   
-        
-      </p> 
-      <span className='font-black text-3xl text-blue-900'> {cliente.nombre}</span>
-      {/* <p className="text-2xl text-gray-700 mt-4">
-        <span className=" uppercase font-bold">Client: </span>
+      <p className="font-black text-4xl text-blue-900">Information about:</p>
+      <span className="font-black text-3xl text-blue-900">
+        {" "}
         {cliente.nombre}
-      </p> */}
-      <p className="text-2xl text-gray-700 mt-4">
+      </span>
+      {/* <p className="text-2xl text-gray-700 mt-4">
+              <span className=" uppercase font-bold">Client: </span>
+              {cliente.nombre}
+            </p> */}
+      <p className="text-2xl text-gray-700 mt-4 font-serif">
         <span className=" uppercase font-bold">Email: </span>
         {cliente.email}
       </p>
-      <p className="text-2xl text-gray-700 mt-4">
+      <p className="text-2xl text-gray-700 mt-4 font-serif">
         <span className=" uppercase font-bold">Phone: </span>
-        {cliente.phone ? `${cliente.phone}` : 'This client has no phone'}
+        {cliente.phone ? `${cliente.phone}` : "This client has no phone"}
       </p>
-      <p className="text-2xl text-gray-700 mt-4">
+      <p className="text-2xl text-gray-700 mt-4 font-serif">
         <span className=" uppercase font-bold">From: </span>
         {cliente.empresa}
       </p>
-      <p className="text-2xl text-gray-700 mt-4">
+      <p className="text-2xl text-gray-700 mt-4 font-serif">
         <span className=" uppercase font-bold">Notes: </span>
-        {cliente.notes ? `${cliente.notes}` : 'This client has no notes'}
+        {cliente.notes ? `${cliente.notes}` : "This client has no notes"}
       </p>
     </div>
+    )
   )
 }
 
-export default VerCliente
+// queres hacer la misma ternaria pero que SOLO APAREZCA si hay notas?
+// {cliente.notes && (
+//   <p>
+//     <span>Notes:</span>
+//     {cliente.notes}
+//   </p>
+// )}
+export default VerCliente;
